571d592246b3ccc47ce4f067a54bcb3765ebd72f
Shannon Malloy <smalloy@linchpinagency.com>
2018-05-03 23:01:19 -0400

    Filter to fix cpt category/tag archive pages

--- functions.php
+++ functions.php
@@ -30,3 +30,16 @@ include_once( 'includes/sjm.php' ); // Theme Class
  */
 
 $theme = new sjm();
+
+add_filter('pre_get_posts', 'query_post_type');
+function query_post_type($query) {
+    if ( is_category() || is_tag() && empty( $query->query_vars['suppress_filters'] ) ) {
+        $post_type = get_query_var('post_type');
+        if($post_type)
+            $post_type = $post_type;
+        else
+            $post_type = array('work','nav_menu_item');
+        $query->set('post_type',$post_type);
+        return $query;
+    }
+}
